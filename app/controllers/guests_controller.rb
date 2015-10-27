2095c52516bb3563843f042d884fbf8fa90beab9
Thomas Petrachi <tpetrachizago@gmail.com>
2015-10-27 22:37:40 +0100

    Add guest

---
+++ app/controllers/guests_controller.rb
@@ -0,0 +1,4 @@
+class GuestsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,11 @@ Rails.application.routes.draw do
   end
 
 
+  controller :guests do
+    get 'hicham' => :index
+  end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
